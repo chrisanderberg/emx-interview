cfc93152b6e6d6362b90f363de4866c49bdc3abf
Chris Anderberg <chris.anderberg@protonmail.com>
2021-02-24 20:32:05 -0800

    Remove unnecessary stuff for project

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(showTimes()))
-  .get('/test', (req, res) => res.send("test"))
+  .get('/', (req, res) => res.send("test"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function showTimes() {
