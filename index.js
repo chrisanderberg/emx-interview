6b00a5386bf0c0e4ffabcbf5fa94f6743e707a1a
Chris Anderberg <chris.anderberg@protonmail.com>
2021-02-24 20:34:31 -0800

    remove all unnecessary paths

--- index.js
+++ index.js
@@ -3,15 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/times', (req, res) => res.send(showTimes()))
   .get('/', (req, res) => res.send("test"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-function showTimes() {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-}
