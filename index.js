c8ef1c2987956d0c692db905c7ee688d5e0df124
Chris Anderberg <chris.anderberg@protonmail.com>
2021-02-24 21:44:28 -0800

    Give some correct answers

--- index.js
+++ index.js
@@ -9,9 +9,43 @@ express()
 
 function generateAnswer(req) {
   let {q, d} = req.query;
-  response = "OK";
+  let response = "";
   if(q) {
     switch(q) {
+      case "Resume":
+        response = "https://mysterious-basin-39601.herokuapp.com/"
+        break;
+      case "Puzzle":
+        response = ""
+        break;
+      case "Position":
+        response = "Software Engineer - Mid Level on the Exchange team"
+        break;
+      case "Phone":
+        response = "(760) 994-5640";
+        break;
+      case "Degree":
+        response = "Bachelor of Science in Computer Science, Minor in Physics at California State Polytechnic University, Pomona";
+        break;
+      case "Referrer":
+        response = "I heard about it from a recruiter, Andrew Rogers.";
+        break;
+      case "Years":
+        response = "I have 8 years of professional experience since graduating, 18 years of writing code since I was a kid.";
+        break;
+      case "Name":
+        response = "Christopher Anderberg";
+        break;
+      case "Source":
+        response = "https://github.com";
+        break;
+      case "Email Address":
+        response = "chris.anderberg@protonmail.com";
+        break;
+      case "Status":
+        response = "yes";
+        break;
+      case "ping":
       default:
         response = "OK";
         break;
